fa3acc2faaed5a840a1e07b6f6dbfde359e716f8
NovemberJoy <novjoy@yahoo.com>
2021-03-25 01:58:30 -0500

    FunKey Build: Updated executable to fix R button crash, resume function added to run.sh

---
+++ build/README.txt
@@ -0,0 +1,16 @@
+MPlayer port for Funkey-S by NovemberJoy
+
+Big thanks to gameblabla, the FunKey team, and gnarface from the mplayer IRC. Without them, this port might not have happened.
+
+Accepted filetypes are .avi, .mpeg, .mp4, .mkv, .webm, and .mov. Resolutions above 240x240 will play, but overly high resolutions may cause performance problems.
+
+Controls:
+
+A - Play/Pause
+B - Toggle on-screen display
+X/Y - Increase/Decrease font size for subtitles
+Up/Down - Increase/Decrease volume
+Fn+Down - Fit video to screen with 1:1 ratio
+Fn+R/Fn+L - Increase/Decrease playback speed
+Left+Right - Seek forward and backwards. Hold Fn to seek more.
+Menu - Exit MPlayer
--- build/run.sh
+++ build/run.sh
@@ -1,17 +1,41 @@
 #!/bin/sh
 
+RESUME=$(dirname "$1")/.$(basename "$1").resume #RESUME script written by steward-fu, used here because MPlayer is GPLv2+ and his script most likely is too.
+
 if [ ! -d "$HOME/.mplayer" ]; then #Make directory if it doesn't exist
 mkdir $HOME/.mplayer
 fi
 
-cp -f input.conf config subfont.ttf $HOME/.mplayer #Use the included input file.
 if [ ! -f "$HOME/.mplayer/setupcomplete" ]; then
 rw
-cp -f liblzo2.so.2 liblzo2.so.2.0.0 /usr/lib #Can we copy necessary libraries using a shell script? I really want to be able to package this as an OPK.
+cp -f liblzo2.so.2 liblzo2.so.2.0.0 /usr/lib #Copy libraries to necessary location for versions of FunKey OS below 2.1.0.
+cp -f input.conf config subfont.ttf $HOME/.mplayer #Use included config and font
 touch $HOME/.mplayer/setupcomplete
 ro
-fi #We will probably need more libraries than this.
-#cp config $HOME/.mplayer/config #Use the included config file.
-#cp subfont.ttf $HOME/.mplayer/subfont.ttf #Use the included TTF font.
+fi
 
-./mplayer -vf scale -x 240 -y 240 "$1" #Try to run mplayer with the selected file?
+#Following script used for resume functionality from steward-fu's Miyoo port of MPlayer. I hope to one day be this good at shell scripting.
+
+if [ -f "$RESUME" ]; then
+	NUM=$(cat -v "$RESUME" | tail -3 | head -1 | sed 's/.*\ V://' | cut -dA -f1 | xargs)
+	if echo "$NUM" | egrep -q '^\-?[0-9]*\.?[0-9]+$'; then
+		echo "This is correct!"
+	else
+		echo "Improperly-formatted RESUME file, removing!"
+		rm "$RESUME"
+	fi
+fi
+
+if [ -f "$RESUME" ]; then
+	END=$(cat -v "$RESUME" | tail -3 | head -1 | sed 's/.*\ V://' | cut -dA -f1 | xargs) #Parses time information from the generated .resume file, but fails if the time information isn't in exactly the right place.
+	./mplayer -ss $END "$1" | tee "$RESUME" #Attempt to seek to correct position.
+else
+    ./mplayer "$1" | tee "$RESUME" #Don't seek with RESUME if it doesn't exist.
+fi
+
+EXIT=$(tail -n 1 "$RESUME") #Get output when MPlayer exits.
+
+if [ "$EXIT" == "Exiting... (End of file)" ]; then
+	echo "End"
+	rm "$RESUME"
+fi
